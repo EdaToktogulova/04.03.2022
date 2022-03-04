c8d602b2c04f67666b7ea5cafb158d255a0c2c47
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-03-04 15:30:13 +0600

    new

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const example1Span = document.querySelector('#example-1 span');
-example1Span.textContent = localStorage.getItem('example-1-number') ?? 0;
-document.querySelector('#example-1 button').addEventListener('click', function() {
-   example1Span.textContent++;
-   localStorage.setItem('example-1-number', example1Span.textContent);
-});
+// const example1Span = document.querySelector('#example-1 span');
+// example1Span.textContent = localStorage.getItem('example-1-number') ?? 0;
+// document.querySelector('#example-1 button').addEventListener('click', function() {
+//    example1Span.textContent++;
+//    localStorage.setItem('example-1-number', example1Span.textContent);
+// });
+// const example2Span = document.querySelector('#example-2 span');
+// example2Span.textContent = localStorage.getItem('example-2-number') ?? 1000;
+// document.querySelector('#example-2 button').addEventListener('click', function() {
+//    example2Span.textContent--;
+//    localStorage.setItem('example-2-number', example2Span.textContent);
+// });
 
 
 // const example2Input = document.querySelector('#example-2 input');
@@ -13,12 +19,26 @@ document.querySelector('#example-1 button').addEventListener('click', function()
 // });
 
 
-const darkTheme = document.querySelector('input[type=checkbox]');
+// const darkTheme = document.querySelector('input[type=checkbox]');
 
-darkTheme.checked = + localStorage.getItem('dark-theme');
-document.body.classList.toggle('dark-theme', darkTheme.checked);
+// darkTheme.checked = + localStorage.getItem('dark-theme');
+// document.body.classList.toggle('dark-theme', darkTheme.checked);
 
-darkTheme.addEventListener('change', function() {
-   localStorage.setItem('dark-theme', +darkTheme.checked);
-   document.body.classList.toggle('dark-theme');
+// darkTheme.addEventListener('change', function() {
+//    localStorage.setItem('dark-theme', +darkTheme.checked);
+//    document.body.classList.toggle('dark-theme');
+// });
+
+
+
+const timeoutExample = setTimeout(function() {
+   document.querySelector('span').textContent = "Hello";
+}, 3000);
+document.querySelector('button').addEventListener('click', function() {
+   clearTimeout(timeoutExample);
+});
+
+
+const timeoutExample = setTimeout(function() {
+   document.querySelector('span').textContent = "Hi";
 });
