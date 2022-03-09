0bb2c379f6aee3fb6d681b554561e469c8eef534
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-03-09 15:21:06 +0600

    added new docs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('main');
 const list  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function displayList() {
+function displayList(list) {
    main.innerHTML = "";
    list.forEach(item => {
       const div = document.createElement('div');
@@ -21,16 +21,27 @@ function sortDescending() {
       }
    });
 }
-document.querySelector('#sort-descending').addEventListener('click', function() {
-   sortDescending();
-   displayList();
-});
-
 function sortAscending() {
    list.sort((a,b) => a> b ? -1: 1);
 };
+function showOdd () {
+   return list.filter(item => % 2 != 0);
+}
 
-function sortOdd( {
-   
+displayList(list);
+
+document.querySelector('#sort-descending').addEventListener('click', function() {
+   sortDescending();
+   displayList(list);
 });
+document.querySelector('#sort-ascending').addEventListener('click', function() {
+   sortAscending();
+   displayList(list);
+});
+document.querySelector('#show-odd').addEventListener('click', function() {
+   displayList(showOdd());
+});
+
+
+
 
