6204ed8a586842f99fc02a007fdf233b39d0ce17
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-03-09 14:27:59 +0600

    new element

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const main = document.querySelector('main');
 const list  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function displayList() {
+   main.innerHTML = "";
    list.forEach(item => {
       const div = document.createElement('div');
       div.textContent = item;
@@ -9,4 +10,9 @@ function displayList() {
    });
 }
 
+function sortDescending() {
+   
+}
+
+sortDescending();
 displayList();
